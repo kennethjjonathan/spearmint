b9140f5ad3cff8302b218f51abf188ba7fe27de8
Kenneth Jonathan <kennethjjonathan@gmail.com>
2023-12-21 18:45:35 +0700

    chore: added prettier for tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import InputWithLabel from "@/components/InputWithLabel";
+
 export default function Home() {
   return (
     <main>
+      <InputWithLabel/>
       <p className="text-foreground">Testttt</p>
       <p className={`font-satoshi text-foreground`}>Testttt</p>
     </main>
---
+++ src/components/InputWithLabel.tsx
@@ -0,0 +1,12 @@
+const InputWithLabel = () => {
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        className="block w-full appearance-none border-0 border-b-2  border-accent bg-transparent px-0 py-1.5 text-accent duration-200 focus:border-white focus:text-foreground focus:outline-none focus:ring-0"
+      />
+    </div>
+  );
+};
+
+export default InputWithLabel;
