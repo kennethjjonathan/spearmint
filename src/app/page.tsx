96189e14d01538e0b15bd3a0702e3969884b7f44
Kenneth Jonathan <kennethjjonathan@gmail.com>
2023-12-29 01:17:35 +0700

    chore: update dev to preview vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/Button";
-import { InputWithLabel } from "@/components/InputWithLabel";
-
 export default function Home() {
   return (
-    <main>
-      <InputWithLabel label="Your Email" type="email" required/>
-      <Button>Test</Button>
+    <main className="container">
     </main>
   );
 }
